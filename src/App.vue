9e78133dd706e276c486157b116580c74f7998d3
Matthew Malone <m.malone@me.com>
2020-01-14 14:02:23 -0700

    Add font to title

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <!-- <div id="app"> -->
+  <div id="app">
     <Header/>
-  <!-- </div> -->
+  </div>
 </template>
 
 <script>
@@ -23,11 +23,8 @@ export default {
     padding: 0;
   }
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Titillium Web', sans-serif;  
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
