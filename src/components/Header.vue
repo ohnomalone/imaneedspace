00f2b084c9ab5aa37ce65401146821c8c1fe6521
Matthew Malone <m.malone@me.com>
2020-01-14 13:02:01 -0700

    Create header component with basic structure

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <header class="header">
+        
+        <h1></h1>
+    </header>
+</template>
+
+
+<script>
+export default {
+    name: 'Header',
+    props: {
+
+    }
+}
+</script>
+
+<style scoped>
+header {
+    color: #000;
+    height: 25px;
+    width: 100%;
+}
+</style>
