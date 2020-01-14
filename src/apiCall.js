1b64b366e934133a934c8820575b246e26c4ef83
Matthew Malone <m.malone@me.com>
2020-01-14 16:03:36 -0700

    Create function to get todays date and month previous date in correct format

---
+++ src/apiCall.js
@@ -0,0 +1,19 @@
+const url = process.env.VUE_APP_NASA_API_URL;
+const apiKey = process.env.VUE_APP_NASA_API_KEY;
+
+export const getSpaceImages = async (startDate, endDate) => {
+    console.log('this is the things: ', url, apiKey);
+    console.log(startDate, endDate);
+    // console.log(Date.now());
+    
+    
+    
+	// const { id } = userInfo;
+	// const url = `{}`;
+	// const response = await fetch(url);
+	// const catalogs = response.json();
+	// if (!response.ok) {
+	// 	return response;
+	// }
+	// return catalogs;
+};
